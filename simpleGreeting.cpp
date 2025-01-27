5580fd38a478962dee0cb9e6e0265b684643aea2
Chee Xiong <107579292+Cheex919@users.noreply.github.com>
2025-01-26 23:24:53 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Cheex on 1/26/2025.
+//
+
+#include "simpleGreeting.h"
+#include <iostream>
+
+int main() {
+  std::string name;
+
+  std::cout <<"Please enter your name: ";
+  std::cin >> name;
+
+  std::cout <<"The name is: "<<name;
+  std::cout <<"\n";
+  return 0;
+  
+}
