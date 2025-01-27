bbf24f150d5e174f4164f215127e1127523c4263
Chee Xiong <107579292+Cheex919@users.noreply.github.com>
2025-01-26 23:21:24 -0800

    Add files via upload

---
+++ sumTwoInts.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Cheex on 1/26/2025.
+//
+
+#include "sumTwoInts.h"
+
+int main() {
+  int n1,n2'
+      ;
+
+  std::cout <<"Please enter the first integer: ";
+  std::cin >> n1;
+
+  std::cout <<"Please enter the second integer: ";
+  std::cin >> n2;
+
+  int sum = n1 + n2;
+  std::cout <<"The sum of the two integers is: "<<sum;
+  std::cout <<"\n";
+  return 0;
+  }
+
+
